e2ef9d0f96219f77c3bd81e36a30a3d1e237f618
Davidsiltroy <davidsiltroy2008@hotmail.com>
2022-04-11 17:58:57 +0300

    exercise Change background color (basic) almost finished, still something to do with the container and the primary color

--- src/exercises/es6/events/bgcolor_advanced.js
+++ src/exercises/es6/events/bgcolor_advanced.js
@@ -1,1 +1,42 @@
+/* eslint-disable */
 // Change background color (advanced)
+
+
+window.onload = function() {
+    console.log('Ready to Work!!');
+    addTheEventListeners();
+};
+
+const addTheEventListeners = () => {
+    let classname1 = "border-gray";
+    let classname2 = "text-color-primary";
+    console.log('Working!!');
+    let listBtns = document.querySelectorAll('div button');
+    let theBody = document.querySelector('body');
+    let theContainer = document.querySelector('div.border-gray');
+
+    theContainer.classList.remove(classname1);
+    theContainer.classList.add(classname2);
+
+    // console.log(listBtns);
+    // console.log(theBody);
+    // console.log(theContainer);
+
+    listBtns.forEach((btn) => {
+        if (btn.textContent == "reset") {
+            btn.addEventListener('click', () => {
+                console.log("toca reiniciar xdd");
+                theBody.setAttribute('style', '');
+
+                theContainer.classList.remove(classname2);
+                theContainer.classList.add(classname1);
+            });
+        } else {
+            btn.addEventListener('click', () => {
+                console.log(btn.textContent);
+                theBody.setAttribute('style', `background-color: ${btn.textContent}`);
+            });
+        }
+
+    });
+}
