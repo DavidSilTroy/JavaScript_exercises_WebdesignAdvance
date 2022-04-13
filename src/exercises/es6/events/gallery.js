9aae2535556b445c0f0e213939e99c5efc4355eb
Davidsiltroy <davidsiltroy2008@hotmail.com>
2022-04-13 23:38:22 +0200

    Finished the exercise Image gallery and all the exercises befor from Event listeners

--- src/exercises/es6/events/gallery.js
+++ src/exercises/es6/events/gallery.js
@@ -1,1 +1,18 @@
+/* eslint-disable */
 // Image gallery
+
+window.onload = function() {
+    console.log('Ready to Work!');
+    let mainImage = document.querySelector('figure img#fullImage');
+    let allImages = document.querySelectorAll('figure#thumbnails img');
+    allImages.forEach(image => {
+        image.addEventListener('click', () => {
+            allImages.forEach(image1 => {
+                image1.classList.remove('border');
+            })
+            console.log(image.getAttribute('data-id'));
+            mainImage.src = `https://picsum.photos/id/${image.getAttribute('data-id')}/600/400`;
+            image.classList.add('border');
+        });
+    });
+};
