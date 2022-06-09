4fc5cc7678d3f6fd58252eead53fe47bf5838802
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-09 20:18:57 +0200

    Done the last Operators exercise, I guess... I did not understand what they asked

--- src/exercises/es6/operators/sentence.js
+++ src/exercises/es6/operators/sentence.js
@@ -1,1 +1,30 @@
+/* eslint-disable */
 // Make a valid sentence
+
+//yep.. I created a object instead of 8 variables..
+const word = {
+    a: "accidentally",
+    b: "server",
+    c: "He",
+    d: "company's",
+    e: "had",
+    f: "hacked",
+    g: "into",
+    h: "his",
+}
+
+const elementForSentence = document.querySelector('p#p-for-sentence');
+const sentence = `
+${word.c} 
+${word.e} 
+${word.a} 
+${word.f} 
+${word.g} 
+${word.h} 
+${word.d} 
+${word.b}.`;
+
+window.onload = function() {
+    console.log('Ready to work!!');
+    elementForSentence.innerHTML = sentence;
+};
