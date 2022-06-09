2f7c599dcd6ec87719fab672f82a759e361d91af
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-09 23:18:01 +0200

    From Math object, done exercise: Lottery

--- src/exercises/es6/math/lottery.js
+++ src/exercises/es6/math/lottery.js
@@ -6,7 +6,8 @@ const ballContainer = document.querySelectorAll('div#ballcontainer div.ball'),
 
 buttonGetNumbers.addEventListener('click', () => {
     ballContainer.forEach((key) => {
-        key.innerHTML = `${Math.round(Math.random() * 45)}`;
+        key.innerHTML = `${Math.round((Math.random() * 44)+1)}`;
         console.log(key.firstChild.data);
     });
 });
+src / exercises / es6 / math / lottery.js
