7013df061448d1c819e88997d55a4ead5e5ee9dc
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-10 00:09:49 +0200

    From Timing, user-defined and arrow functions, done exercise: Random image

--- src/exercises/es6/functions/randomImage.js
+++ src/exercises/es6/functions/randomImage.js
@@ -1,1 +1,19 @@
+/* eslint-disable */
 // Random image
+
+const imageContainer = document.querySelector('main.container div.border-green');
+
+const randomMinMax10 = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min) * 100;
+};
+
+let timer = setInterval(() => {
+    document.querySelector('main.container div.border-green figure').remove();
+    let imgSrc = `https://picsum.photos/${randomMinMax10(1,8)}/${randomMinMax10(1,4)}`;
+    imageContainer.innerHTML = `<figure>
+    <figcaption>
+        <h3>${imgSrc}</h3>
+    </figcaption>
+    <img src="${imgSrc}" alt="Random image">
+</figure>`;
+}, 5000);
