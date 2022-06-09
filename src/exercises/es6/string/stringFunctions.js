0b0d57a920b8ac940a687fdbad7e75f7771089c0
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-09 21:53:22 +0200

    From String Object, done exercise: String functions

--- src/exercises/es6/string/stringFunctions.js
+++ src/exercises/es6/string/stringFunctions.js
@@ -1,2 +1,37 @@
+/* eslint-disable */
 // String functions
 const word = 'ihznduq hmpibnughpimhg yfawozhbduqzg dlwbubkbauk';
+
+const blockContainer = document.querySelectorAll('div.border-green div.block');
+
+const doSomething = (option) => {
+    switch (option) {
+        case 0:
+            return word.charAt(word.indexOf('w') + 1).toUpperCase();
+            break;
+        case 1:
+            return word.lastIndexOf('w');
+            break;
+        case 2:
+            if (word.indexOf('x') == -1) return false;
+            return true;
+            break;
+        case 3:
+            return word.length;
+            break;
+        case 4:
+            return word.replaceAll(' ', '').length;
+            break;
+
+        default:
+            return 'nothing to do';
+            break;
+    }
+};
+
+let optionNumb = 0;
+
+blockContainer.forEach((key) => {
+    key.innerHTML = `${doSomething(optionNumb)}`;
+    optionNumb++;
+});
