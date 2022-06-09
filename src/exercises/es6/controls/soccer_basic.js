d16191b417d716a6c2273501bcd0d75861fbcb0f
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-09 20:41:13 +0200

    From Control structure, done exercise: A soccer game (basic)

--- src/exercises/es6/controls/soccer_basic.js
+++ src/exercises/es6/controls/soccer_basic.js
@@ -1,2 +1,29 @@
+/* eslint-disable */
 // A soccer game (basic)
 const score = ['Belgium', 'Spain', 'Spain', 'Belgium', 'Spain', 'Belgium', 'Belgium', 'Belgium'];
+
+const table = document.querySelector('table#soccer tbody');
+
+let dataTable = '';
+let belgiumScore = 0;
+let spainScore = 0;
+
+score.forEach((key) => {
+    if (key == 'Belgium') {
+        belgiumScore += 1;
+    } else {
+        spainScore++;
+    }
+
+    dataTable +=
+        `<tr>
+            <td>
+            ${belgiumScore}
+            </td>
+            <td>
+            ${spainScore}
+            </td>
+        </tr>`;
+});
+
+table.innerHTML = dataTable;
