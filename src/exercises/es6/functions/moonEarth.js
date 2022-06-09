2b959790d85886a4c89188f58d5b23bdaa288ea5
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-10 00:44:59 +0200

    From Timing, user-defined and arrow functions, done exercise: Moon and earth movement

--- src/exercises/es6/functions/moonEarth.js
+++ src/exercises/es6/functions/moonEarth.js
@@ -1,1 +1,31 @@
+/* eslint-disable */
 // Moon and earth movement
+
+const moonEarthImg = document.querySelector('img#image');
+
+let i = 0;
+let timeDelay = 24;
+
+const showNextImage = () => {
+
+    let number = i.toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGrouping: false
+    })
+    moonEarthImg.src = `../../../assets/moonEarth/IMG${number}.gif`;
+    i++;
+    i = i > 23 ? 0 : i;
+
+    if (parseInt(document.querySelector('input#delay').value) != timeDelay) {
+        clearInterval(timer);
+        timeDelay = parseInt(document.querySelector('input#delay').value);
+        timer = setInterval(showNextImage, timeDelay);
+    }
+};
+
+let timer = setInterval(showNextImage, timeDelay);
+
+
+
+
+//<img id="image" src="../../../assets/moonEarth/IMG00.gif" alt="moon-earth"></img>
