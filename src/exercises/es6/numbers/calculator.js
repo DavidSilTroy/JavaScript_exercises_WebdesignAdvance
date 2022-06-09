0887e36748aa6ddeca4851f9466565a2f9f7e195
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-09 22:17:10 +0200

    From Number Object, done exercise: Calculator

--- src/exercises/es6/numbers/calculator.js
+++ src/exercises/es6/numbers/calculator.js
@@ -1,1 +1,44 @@
+/* eslint-disable */
 //  Calculator
+
+const mathButtons = document.querySelectorAll('div#buttons button'),
+    resultContainer = document.querySelector('p#result');
+
+let number1 = document.querySelector('input#number1'),
+    number2 = document.querySelector('input#number2');
+
+const addListenerToButton = (btn, value) => {
+    switch (value) {
+        case 'Add':
+            btn.addEventListener('click', () => {
+                let result = parseInt(number1.value) + parseInt(number2.value);
+                resultContainer.innerHTML = `${result}`;
+            });
+            break;
+        case 'Subtract':
+            btn.addEventListener('click', () => {
+                let result = parseInt(number1.value) - parseInt(number2.value);
+                resultContainer.innerHTML = `${result}`;
+            });
+            break;
+        case 'Multiply':
+            btn.addEventListener('click', () => {
+                let result = parseInt(number1.value) * parseInt(number2.value);
+                resultContainer.innerHTML = `${result}`;
+            });
+            break;
+        case 'Divide':
+            btn.addEventListener('click', () => {
+                let result = parseInt(number1.value) / parseInt(number2.value);
+                resultContainer.innerHTML = `${result}`;
+            });
+            break;
+        default:
+            break;
+    }
+};
+
+mathButtons.forEach((key) => {
+    console.log(key.firstChild.data);
+    addListenerToButton(key, key.firstChild.data);
+});
