e9cf32a5de2344c961a3e7cbcedd46d6f157d315
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-09 11:43:47 +0200

    Done with the sales exercise in the Operators section

--- src/exercises/es6/operators/feet.js
+++ src/exercises/es6/operators/feet.js
@@ -1,16 +1,8 @@
 /* eslint-disable */
 // Centimeters to Feet
 
-// Create a centimeters to feet converter
-
-// Step 1: create a variable centimeters with value 60
-// Step 2: create a variable factor with value 0.0328084 (1 cm = 0.0328084 feet)
-// Step 3: calculate the corresponding length in feet and assign it to a variable feet
-// Step 4: replace the content of the span tags with the value of the centimeters and feet variables
-// Step 5: round the result to 2 digits after the comma
-
-var x;
-var y;
+let x;
+let y;
 
 const centimeters = 60;
 const factor = 0.0328084;
@@ -22,16 +14,14 @@ window.onload = function() {
         const feets = calculateFeetFromCentimeters(centimeters);
         showValuesInPage(feets);
     } else {
-        console.log('Not elements found')
+        console.log('Not elements found');
     }
 };
 
 const getElementsToChange = () => {
     x = document.querySelector('span#centimeters');
     y = document.querySelector('span#feet');
-
     const ready = x && y != null ? true : false;
-    // console.log(`x = ${x} and y = ${y}`)
     return ready;
 };
 
--- src/exercises/es6/operators/sales.js
+++ src/exercises/es6/operators/sales.js
@@ -1,1 +1,52 @@
+/* eslint-disable */
 //  Sales
+
+const itemsShop = {
+    Trousers: 85,
+    Sweater: 62,
+    Coat: 120,
+    Shirt: 25,
+    Shoes: 105,
+};
+const webShop = {
+    discount: 70,
+    bill: 0,
+    valueElementID: 'span#bill',
+    totalValueElement: null,
+};
+
+const checkValueElement = () => (
+    webShop.totalValueElement != null ||
+    webShop.totalValueElement != undefined
+);
+
+const getValueElement = (_callback) => {
+    webShop.totalValueElement = document.querySelector(webShop.valueElementID);
+    _callback(setTotalAmountToPay);
+};
+const calculateTotalAmountToPay = (_callback) => {
+    let valueToPay = 0;
+    let discount = 0;
+    Object.keys(itemsShop).forEach((key) => {
+        valueToPay += itemsShop[key];
+    });
+    console.log('total:', valueToPay);
+    discount = (valueToPay * webShop.discount / 100);
+    console.log('discount:', discount);
+    webShop.bill = Math.round(valueToPay - discount); // -> round to the nearest int
+    console.log('total with discount:', webShop.bill);
+    _callback();
+};
+const setTotalAmountToPay = () => {
+    webShop.totalValueElement.innerHTML = webShop.bill;
+    console.log('Done!!');
+};
+
+window.onload = function() {
+    console.log('Ready to work!!');
+    if (checkValueElement()) {
+        calculateTotalAmountToPay(setTotalAmountToPay);
+    } else {
+        getValueElement(calculateTotalAmountToPay);
+    }
+};
