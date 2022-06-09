0846a8d731abe5f35ad8a6546e3334d6301a5cc7
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-09 20:27:55 +0200

    From Control structure, done exercise: did I pass?

--- src/exercises/es6/controls/pass.js
+++ src/exercises/es6/controls/pass.js
@@ -1,1 +1,10 @@
+/* eslint-disable */
 // Did I pass?
+
+let result = 16;
+
+if (result >= 10) {
+    document.querySelector('i.fas.fa-check').style.display = 'block';
+} else {
+    document.querySelector('i.fas.fa-times-circle').style.display = 'block';
+}
