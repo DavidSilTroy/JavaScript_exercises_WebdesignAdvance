0dbf68d3659b597789e4fa06463dd822e3aa6223
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-09 23:50:23 +0200

    From Timing, user-defined and arrow functions, done exercise: Slideshow

--- src/exercises/es6/functions/slideshow.js
+++ src/exercises/es6/functions/slideshow.js
@@ -1,1 +1,8 @@
+/* eslint-disable */
 // Slideshow
+const imagesContainer = document.querySelector('ul#album');
+let timer = setInterval(() => {
+    // execute this code every 3 seconds
+    imagesContainer.appendChild(imagesContainer.children[0]);
+    console.log(imagetoMove);
+}, 3000);
