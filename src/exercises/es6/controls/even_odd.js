12891102bf662483d4df3200850ae84cef8f7e0e
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-09 21:05:54 +0200

    From Control structure, done exercise: Fibonacci

--- src/exercises/es6/controls/even_odd.js
+++ src/exercises/es6/controls/even_odd.js
@@ -1,1 +1,31 @@
+/* eslint-disable */
 // Even or odd?
+
+const evenContainer = document.querySelector('div#numbers div.border-green ul');
+const oddContainer = document.querySelector('div#numbers div.border-red ul');
+const primeContainer = document.querySelector('div#numbers div.border-blue ul');
+
+let evenToInner = '';
+let oddToInner = '';
+let primeToInner = '';
+
+const isPrime = (num) => {
+    for (var i = 2; i < num; i++)
+        if (num % i === 0) return false;
+    return num > 1;
+};
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+        evenToInner += `<li> ${i} </li>`;
+    } else {
+        oddToInner += `<li> ${i} </li>`;
+    }
+    if (isPrime(i)) {
+        primeToInner += `<li> ${i} </li>`;
+    }
+}
+
+evenContainer.innerHTML = evenToInner;
+oddContainer.innerHTML = oddToInner;
+primeContainer.innerHTML = primeToInner;
